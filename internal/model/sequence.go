48ab246a0455a9c21d9c12ed38562f49471daefe
Cattle_Horse <cattle_horse@qq.com>
2025-03-11 17:24:11 +0800

    docs: 更新序列号表注释

--- internal/model/sequence.go
+++ internal/model/sequence.go
@@ -1,7 +1,7 @@
 package model
 
 // Sequence 序列号表
-// 用于TDDL发号器
+// 用于数据库号段发号器
 type Sequence struct {
 	Model
 	Name     string `json:"name" gorm:"type:varchar(500);not null;uniqueIndex:idx_sequence_name;comment:序列名称"`
