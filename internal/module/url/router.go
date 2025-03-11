dc918a22af152db8a10b83301cc26bddb26a3d6f
Cattle_Horse <cattle_horse@qq.com>
2025-03-11 17:23:27 +0800

    ✨feat(core): 添加限流中间件并控制鉴权中间件的启用

--- internal/module/url/router.go
+++ internal/module/url/router.go
@@ -1,18 +1,24 @@
 package url
 
 import (
+	"github.com/Cattle0Horse/url-shortener/config"
 	"github.com/Cattle0Horse/url-shortener/internal/global/middleware"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
 )
 
 func (u *ModuleUrl) InitRouter(r *gin.RouterGroup) {
-	r.GET("/api/url/:code", Redirect) // 重定向到原始链接
-	r.GET("/:code", Redirect)         // 重定向到原始链接
+	r.Group("", middleware.RateLimiter(rate.Limit(
+		config.Get().Server.StandAloneReadRate), config.Get().Server.StandAloneReadBurst)).
+		GET("/api/url/:code", Redirect). // 重定向到原始链接
+		GET("/:code", Redirect)          // 重定向到原始链接
 
-	//添加鉴权中间件
-	r.Group("/api", middleware.Auth()).
-		GET("/urls", FetchAll).       // 获取用户的所有短链接
-		POST("/url", Create).         // 创建短链接
-		DELETE("/url/:code", Delete). // 删除短链接
-		PATCH("/url/:code", Update)   // 更新短链接的有效期
+	if !config.Get().Server.Readonly {
+		//添加鉴权中间件
+		r.Group("/api", middleware.Auth()).
+			GET("/urls", FetchAll).       // 获取用户的所有短链接
+			POST("/url", Create).         // 创建短链接
+			DELETE("/url/:code", Delete). // 删除短链接
+			PATCH("/url/:code", Update)   // 更新短链接的有效期
+	}
 }
