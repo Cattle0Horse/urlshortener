d76cbaed7bc7b149b72e5757d0dbf56b16cbf4e3
Cattle_Horse <cattle_horse@qq.com>
2025-03-11 17:25:35 +0800

    docs: 更新README中的待办事项，并对注释进行增加和修改

--- internal/model/url.go
+++ internal/model/url.go
@@ -6,7 +6,7 @@ import (
 
 type Url struct {
 	Model
-	ShortCode   string    `json:"short_code" gorm:"type:varchar(255);not null;uniqueIndex:idx_url_short_code;collate:utf8mb4_bin;comment:短链接"`
+	ShortCode   string    `json:"short_code" gorm:"type:varchar(16);not null;uniqueIndex:idx_url_short_code;collate:utf8mb4_bin;comment:短链接"`
 	OriginalUrl string    `json:"original_url" gorm:"type:varchar(255);not null;comment:原始链接"`
 	ExpiryTime  time.Time `json:"expiry_time" gorm:"type:time;not null;comment:过期时间"`
 	UserID      uint      `json:"user_id" gorm:"not null;comment:创建用户ID"`
